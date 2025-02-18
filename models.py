5649ad4b654792bbed808f72262f38735f840b86
Gabriel Siqueira <gabrielsiqueira@Gabriels-MacBook-Pro.local>
2025-02-17 21:37:58 -0300

    adicionando rotas e organizando as pastas

--- models.py
+++ models.py
@@ -9,4 +9,4 @@ class Todos(Base):
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    Complete = Column(Boolean, default=False)
+    complete = Column(Boolean, default=False)
