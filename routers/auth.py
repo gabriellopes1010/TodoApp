16ea2ce433a3b81279e8ae3d899c9423e6fb4938
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-02-20 16:53:58 -0300

    adicioanando a page de register

--- routers/auth.py
+++ routers/auth.py
@@ -56,6 +56,11 @@ def render_login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
+@router.get("/register-page")
+def render_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
 ##ENDPOINTS
 def authenticate_user(username: str, password: str, db):
     user = db.query(Users).filter(Users.username == username).first()
