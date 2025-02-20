06467933cffbe4c8c3016545c5ea097f0047f42a
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-02-20 09:38:01 -0300

    adicionando os testes de auth

--- test/test_users.py
+++ test/test_users.py
@@ -29,8 +29,13 @@ def test_change_password_success(test_user):
 def test_change_password_invalid_current_password(test_user):
     response = client.put(
         "/user/password",
-        json={"password": "testpassword", "new_password": "newpassword"},
+        json={"password": "wrong_password", "new_password": "newpassword"},
     )
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {"detail": "error on password change"}
+    assert response.json() == {"detail": "Error on password change"}
+
+
+def test_change_phone_number_succsess(test_user):
+    response = client.put("/user/phonenumber/222222222222")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
