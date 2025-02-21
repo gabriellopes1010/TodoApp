06f0e7a099ae15a55627ce0b80cfa28e4c81acbd
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-02-21 17:04:26 -0300

    tentando subindo com o docker

--- src/database.py
+++ src/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:noXxnurvRPUEgcfUooxlAJaIFhFXYrBk@shinkansen.proxy.rlwy.net:58780/railway"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://gabriellopes1010:teste1234@localhost:5433/TodoApplicationDatabase"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
