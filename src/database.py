0d3692dfa41649238c81693971d6ebc45cc235c7
Gabriel Siqueira <gabrielsiqueira@Gabriels-MacBook-Pro.local>
2025-02-23 22:18:03 -0300

    adicionando o .env e ajustes

--- src/database.py
+++ src/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://gabriellopes1010:teste1234@localhost:5433/TodoApplicationDatabase"
-)
+load_dotenv()
 
+# Construa a URL de conexão usando as variáveis de ambiente
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
